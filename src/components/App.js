3c162a822119ad1da01e1b375a86bd2564541d9a
piotrfijol <piotr.fijol1@gmail.com>
2021-06-11 12:30:27 +0200

    Added window name bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
+import '../styles/css/App.css';
+import logo from '../icons/logo.png';
+
 function App() {
   return (
     <div className="container">
+      <div className="frame-name">
+        <div className="frame-name__content">
+          <img src={logo} alt="logo" id="logo" className="icon" />
+          <p>frameName - PaintJS</p>
+        </div>
+      </div>
     </div>
   );
 }
