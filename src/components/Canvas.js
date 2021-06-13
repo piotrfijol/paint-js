957dbe4658b8d7d525688e558d09e46ee2b850af
piotrfijol <piotr.fijol1@gmail.com>
2021-06-13 19:39:45 +0200

    Made canvas resizable

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,31 +3,113 @@ import React, { createRef, useEffect, useState } from 'react'
 export default function Canvas({selectedTool: tool}) {
 
     const [isDragging, setDrag] = useState(false);
+    const [offset, setOffset] = useState({x: null, y: null});
+    const [ctx, setCtx] = useState(null);
+    const [width, setWidth] = useState(250);
+    const [height, setHeight] = useState(250);
     let canvasRef = createRef();
-    let ctx = createRef();
+    let canvasWrapperRef = createRef();
 
     useEffect(() => {
       if(canvasRef.current) {
-        ctx.current = canvasRef.current.getContext("2d");
+        setCtx(canvasRef.current.getContext("2d"));
+
+        const el = canvasRef.current.getBoundingClientRect();
+        setOffset({
+          x: el.left,
+          y: el.top
+        });
       }
     }, []);
 
-    const handleClick = e => {
-      setDrag(!isDragging);
-      console.log(isDragging);
+    const handleCanvasMouseDown = e => {
+      setDrag(true);
+      ctx.beginPath();
+      ctx.moveTo(e.clientX - offset.x, e.clientY - offset.y);
     }
 
+    const handleCanvasMouseUp = e => {
+      setDrag(false);
+    }
+
+
     const handleMouseMove = e => {
-      console.log("tak")
-      if(ctx.current !== null && isDragging === true) {
-        ctx.current.fillStyle = "green";
-        ctx.current.fillRect(0, 0, 250, 250);
+      if(ctx !== null && isDragging === true) {
+        ctx.lineTo(e.clientX - offset.x, e.clientY - offset.y);
+        ctx.stroke();
       }
     }
 
+    const handleResize = (item, e) => {
+      if(item.contains("y-resize"))
+        setHeight(e.clientY-offset.y);
+      else if(item.contains("xy-resize")) {
+        setHeight(e.clientY-offset.y);
+        setWidth(e.clientX-offset.x);
+      } else if (item.contains("x-resize")) {
+        setWidth(e.clientX-offset.x);
+      }
+    }
+
+    let handler;
+
+    const startResizingY = e => {
+      e.preventDefault();
+      handler = handleResize.bind(null, e.target.classList);
+      window.addEventListener('mousemove', handler);
+      window.addEventListener('mouseup', finishResizing);
+    }
+
+    const startResizingX = e => {
+      e.preventDefault();
+      handler = handleResize.bind(null, e.target.classList);
+      window.addEventListener('mousemove', handler);
+      window.addEventListener('mouseup', finishResizing);
+    }
+
+    const startResizingXY = e => {
+      e.preventDefault();
+      handler = handleResize.bind(null, e.target.classList);
+      window.addEventListener('mousemove', handler);
+      window.addEventListener('mouseup', finishResizing);
+    }
+
+    const finishResizing = e => {
+      window.removeEventListener('mousemove', handler);
+      window.removeEventListener('mouseup', finishResizing);
+    }
+
     return (
-        <div className="canvas">
-          <canvas ref={canvasRef} onClick={handleClick} onMouseMove={handleMouseMove}></canvas>
+        <div className="canvas-container"  onMouseUp={handleCanvasMouseUp}   >
+          <div width={width} height={height} ref={canvasWrapperRef} className="canvas" >
+            <canvas width={width} height={height} ref={canvasRef} 
+              onMouseDown={handleCanvasMouseDown} 
+              onMouseMove={handleMouseMove}
+              onMouseOut={handleCanvasMouseUp}
+            />
+            
+          <div className="y-resize resize" 
+            style = {{
+              left: width/2,
+              top: height+8
+            }}
+            onMouseDown={startResizingY}
+          />
+          <div className="xy-resize resize" 
+            style = {{
+              left: width,
+              top: height+8
+            }}
+            onMouseDown={startResizingXY}
+          />
+          <div className="x-resize resize" 
+            style = {{
+              left: width+8,
+              top: height/2
+            }}
+            onMouseDown={startResizingX}
+          />
+          </div>
         </div>
     )
 }
